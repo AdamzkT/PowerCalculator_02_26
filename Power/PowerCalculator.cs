4f43d4bd9f5fe6fcc5bbe430a1383c7b17fd5b3c
AdamzkT <91147017+AdamzkT@users.noreply.github.com>
2025-02-26 11:59:09 +0100

    Add files via upload

---
+++ Power/PowerCalculator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Power
+{
+    public class PowerCalculator
+    {
+        public double CalculatePower(int basee, int exponent)
+        {
+            double eredmeny = 1;
+            if(exponent >= 0)
+            {
+                for (int i = 0; i < exponent; i++)
+                {
+                    eredmeny = eredmeny * basee;
+                }
+            }
+            else
+            {
+                if (basee == 0) { return 0; }
+                for (int i = 0; i < -exponent; i++)
+                {
+                    eredmeny = eredmeny / basee;
+                }
+            }
+            
+            return eredmeny;
+        }
+
+        static void Main(string[] args)
+        {
+            var power = new PowerCalculator();
+            int szam = intBemenet("szám: ");
+            int hatvany = intBemenet("hatvány: ");
+            Console.WriteLine("Eredmény: " + power.CalculatePower(szam, hatvany));
+
+            Console.ReadKey();
+        }
+
+        private static int intBemenet(string szoveg)
+        {
+            while (true) {
+                try
+                {
+                    Console.Write(szoveg);
+                    int be = Convert.ToInt32(Console.ReadLine());
+                    return be;
+                }
+                catch { Console.WriteLine("  Rossz bemenet"); }
+            }
+        }
+    }
+}
---
+++ PowerTests/PowerCalculatorTests.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Power;
+
+namespace PowerTests
+{
+    [TestClass]
+    public class PowerCalculatorTests
+    {
+        [TestMethod]
+        public void TestPositiveExponent()
+        {
+            var power = new PowerCalculator();
+            int a = 2;
+            int b = 3;
+            double eredmeny = 8;
+
+            double result = power.CalculatePower(a, b);
+
+            Assert.AreEqual(eredmeny, result);
+        }
+
+        [TestMethod]
+        public void TestZeroExponent()
+        {
+            var power = new PowerCalculator();
+            int a = 5;
+            int b = 0;
+            double eredmeny = 1;
+
+            double result = power.CalculatePower(a, b);
+
+            Assert.AreEqual(eredmeny, result);
+        }
+
+        [TestMethod]
+        public void TestNegativeExponent()
+        {
+            var power = new PowerCalculator();
+            int a = 2;
+            int b = -2;
+            double eredmeny = 0.25;
+
+            double result = power.CalculatePower(a, b);
+
+            Assert.AreEqual(eredmeny, result);
+        }
+
+        [TestMethod]
+        public void TestZeroBasePositiveExponent()
+        {
+            var power = new PowerCalculator();
+            int a = 0;
+            int b = 5;
+            double eredmeny = 0;
+
+            double result = power.CalculatePower(a, b);
+
+            Assert.AreEqual(eredmeny, result);
+        }
+
+        [TestMethod]
+        public void TestZeroBaseZeroExponent()
+        {
+            var power = new PowerCalculator();
+            int a = 0;
+            int b = 0;
+            double eredmeny = 1;
+
+            double result = power.CalculatePower(a, b);
+
+            Assert.AreEqual(eredmeny, result);
+        }
+
+        [TestMethod]
+        public void TestOneBaseAnyExponent()
+        {
+            var power = new PowerCalculator();
+            int a = 1;
+            int b = 100;
+            double eredmeny = 1;
+
+            double result = power.CalculatePower(a, b);
+
+            Assert.AreEqual(eredmeny, result);
+        }
+
+        [TestMethod]
+        public void TestNegativeBaseEvenExponent()
+        {
+            var power = new PowerCalculator();
+            int a = -3;
+            int b = 4;
+            double eredmeny = 81;
+
+            double result = power.CalculatePower(a, b);
+
+            Assert.AreEqual(eredmeny, result);
+        }
+
+        [TestMethod]
+        public void TestNegativeBaseOddExponent()
+        {
+            var power = new PowerCalculator();
+            int a = -2;
+            int b = 3;
+            double eredmeny = -8;
+
+            double result = power.CalculatePower(a, b);
+
+            Assert.AreEqual(eredmeny, result);
+        }
+
+        [TestMethod]
+        public void TestNegativeBaseNegativeExponent()
+        {
+            var power = new PowerCalculator();
+            int a = -2;
+            int b = -2;
+            double eredmeny = 0.25;
+
+            double result = power.CalculatePower(a, b);
+
+            Assert.AreEqual(eredmeny, result);
+        }
+    }
+}
